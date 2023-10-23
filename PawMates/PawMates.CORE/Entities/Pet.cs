955f98de755e54afee5fdc09912cb25482ccf4dc
Tiara Bell <hello@tiarabell.com>
2023-10-23 16:46:44 -0400

    Add Pet,Event,PlayDate Entities and DTOs

---
+++ PawMates/PawMates.CORE/Entities/Pet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.CORE.Entities
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public int PetTypeId { get; set; }
+        public string Name { get; set; }
+        public string? Breed { get; set; }
+        public int Age { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
---
+++ PawMates/PawMates.CORE/Entities/PlayDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.CORE.Entities
+{
+    public class PlayDate
+    {
+        public int Id { get; set; }
+        public int PetParentId { get; set; }
+        public int LocationId { get; set; }
+        public int EventTypeId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+    }
+}
