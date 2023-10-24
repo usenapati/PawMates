f11b06fe0737140de206a516cb80083aab0b8dcc
James7z <james7.ca@gmail.com>
2023-10-24 10:17:11 -0400

    add EFRepositoty<T> and DALException

---
+++ PawMates/PawMates.CORE/Exceptions/DALException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.CORE.Exceptions
+{
+    [Serializable]
+    public class DALException : Exception
+    {
+
+        public DALException(string? message) : base(message)
+        {
+        }
+
+        public DALException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
