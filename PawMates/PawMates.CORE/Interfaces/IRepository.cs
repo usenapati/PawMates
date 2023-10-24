1ab93d20c9a9a750ec6945f38f6ac4e254b7c601
James7z <james7.ca@gmail.com>
2023-10-23 22:33:53 -0400

    add IEntity and IRepository

---
+++ PawMates/PawMates.CORE/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.CORE.Interfaces
+{
+    public interface IRepository<T> where T : class, IEntity
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetAll(Func<T, bool> predicate);
+        T? GetOne(Func<T, bool> predicate);
+    }
+}
