f15803e6a3f3101fd78a5c843053e7953ba7c3df
James7z <james7.ca@gmail.com>
2023-10-23 22:20:27 -0400

    fix merge conflicts

---
+++ PawMates/PawMates.CORE/DTOs/PetParentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PawMates.CORE.DTOs
+{
+    public class PetParentDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
+
---
+++ PawMates/PawMates.CORE/DTOs/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PawMates.CORE.DTOs
+{
+    public class UserDTO
+    {
+        [Required]
+        public int Id { get; set; }
+        public int PetParentId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+    }
+}
