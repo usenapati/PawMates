98c90d45426a33bff108e684d78892bdc0c08563
James7z <james7.ca@gmail.com>
2023-10-24 12:02:29 -0400

    complete PetApi

--- PawMates/PawMates.CORE/Entities/User.cs
+++ PawMates/PawMates.CORE/Entities/User.cs
@@ -7,13 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PawMates.DAL.Models;
+namespace PawMates.DAL.Models
 {
     public class User : IEntity
 {
         public int Id { get; set; }
         public int PetParentId { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
 
         public virtual PetParent PetParent { get; set; } = null!;
