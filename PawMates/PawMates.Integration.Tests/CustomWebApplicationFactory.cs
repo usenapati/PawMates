8ae17b331e736ba96daa3923401f502158fddc25
Tiara Bell <hello@tiarabell.com>
2023-10-25 12:53:11 -0400

    Add CustomWebAppFactory and Packages for Integration Tests

---
+++ PawMates/PawMates.Integration.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,40 @@
+﻿using PawMates.DAL;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.Integration.Tests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<PawMatesContext>));
+
+                if (dbContextDescriptor != null)
+                {
+                    services.Remove(dbContextDescriptor);
+                }
+
+                services.AddDbContext<PawMatesContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+
+                });
+            });
+
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
