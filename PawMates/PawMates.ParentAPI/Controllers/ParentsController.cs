21c1cd70e4aebccd799a432144dd7a7b21fbb3ab
Tiara Bell <hello@tiarabell.com>
2023-10-25 11:49:30 -0400

    Update ParentRepository reference on ParentController

--- PawMates/PawMates.ParentAPI/Controllers/ParentsController.cs
+++ PawMates/PawMates.ParentAPI/Controllers/ParentsController.cs
@@ -12,9 +12,9 @@ namespace PawMates.ParentAPI.Controllers
     [ApiController]
     public class ParentsController : ControllerBase
     {
-        private readonly EFRepository<PetParent> _parentRepository;
+        private readonly EFParentRepository _parentRepository;
 
-        public ParentsController(EFRepository<PetParent> parentRepository)
+        public ParentsController(EFParentRepository parentRepository)
         {
             _parentRepository = parentRepository;
         }
@@ -118,7 +118,7 @@ namespace PawMates.ParentAPI.Controllers
                 {
                     return NotFound();
                 }
-                _parentRepository.Delete(parent);
+                _parentRepository.Delete(parent.Id);
                 return NoContent();
             }
             catch (Exception ex)
