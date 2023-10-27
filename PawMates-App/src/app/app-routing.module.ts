8f2f80b5fc44a1e8bf77ced82c800ad16c0804f4
Udayan Senapati <udayansenapati@gmail.com>
2023-10-27 11:54:00 -0400

    Add Routes to Nav Bar
    Need to add TS function for hiding/displaying elements - tied to login

--- PawMates-App/src/app/app-routing.module.ts
+++ PawMates-App/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AppComponent } from './app.component';
+import { PetparentDetailComponent } from './components/petparent/petparent-detail/petparent-detail.component';
+import { PetsComponent } from './components/pet/pets/pets.component';
+import { PlaydatesComponent } from './components/playdate/playdates/playdates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent }, // Change it to a separate component
+  {path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent },
+
+  {path: 'profile', component: PetparentDetailComponent },
+
+  {path: 'pets', component: PetsComponent },
+
+  {path: 'events', component: PlaydatesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
