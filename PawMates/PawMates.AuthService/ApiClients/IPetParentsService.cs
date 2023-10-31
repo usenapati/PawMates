7ec77cb1b4221d2642574ade00cfeb94a6b14521
James7z <james7.ca@gmail.com>
2023-10-31 11:09:14 -0400

    add useDTO and petParentDTO in login return; Tested in api

---
+++ PawMates/PawMates.AuthService/ApiClients/IPetParentsService.cs
@@ -0,0 +1,11 @@
+﻿using PawMates.CORE.DTOs;
+
+namespace PawMates.AuthService.ApiClients
+{
+    public interface IPetParentsService
+    {
+        Task<List<PetParentDTO>?> GetPetParentsAsync();
+
+        Task<PetParentDTO>? GetPetParentAsync(int petParentId);
+    }
+}
