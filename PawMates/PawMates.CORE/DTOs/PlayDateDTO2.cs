e626fd6047fc1f695ab7dfb8e7cd1890ece1b44f
James7z <james7.ca@gmail.com>
2023-11-01 11:02:32 -0400

    create PlayDateDTO2 for front-end; Tested in API

---
+++ PawMates/PawMates.CORE/DTOs/PlayDateDTO2.cs
@@ -0,0 +1,24 @@
+﻿using PawMates.CORE.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawMates.CORE.DTOs
+{
+    //PlayDateDTO only for output for front-end
+    public class PlayDateDTO2
+    {
+        public int Id { get; set; }
+        public string HostName { get; set; }
+        public string LocationName { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int NumberOfPets { get; set; }
+        public virtual ICollection<PetDTO> Pets { get; set; } = new List<PetDTO>();
+    }
+}
