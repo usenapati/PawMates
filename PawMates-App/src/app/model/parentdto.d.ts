c73da5c00824c66aa36ad8661beec02fe568635b
Tiara Bell <hello@tiarabell.com>
2023-11-02 22:33:57 -0400

    Add form validation to Update Parent

---
+++ PawMates-App/src/app/model/parentdto.d.ts
@@ -0,0 +1,11 @@
+export interface ParentDTO {
+    firstName: string,
+    lastName: string,
+    email: string,
+    phoneNumber: string,
+    imageUrl?: string,
+    description: string,
+    city: string,
+    state: string,
+    postalCode: string
+}
